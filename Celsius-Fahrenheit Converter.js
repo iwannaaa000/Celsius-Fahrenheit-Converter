2d0c4409829b2d230e81075d87c6f608c1bc7e3e
iwannaaa000 <jackiegourna@gmail.com>
2025-03-26 22:06:58 +0200

    .

--- Celsius-Fahrenheit Converter.js
+++ Celsius-Fahrenheit Converter.js
@@ -1,17 +1,15 @@
-
-
 function convertTemperature() {
-    let temperature = document.getElementById("temperature").value;
-    let unit = document.getElementById("unit").value;
-    let temperatureResult = document.getElementById("temperatureResult");
+  let temperature = document.getElementById("temperature").value;
+  let unit = document.getElementById("unit").value;
+  let temperatureResult = document.getElementById("temperatureResult");
 
-    if (unit === "celsius") {
-        let celsius = parseFloat(temperature);
-        let fahrenheit = (celsius * 9/5 ) + 32;
-        temperatureResult.textContent = `Result: ${fahrenheit.toFixed(2)} °F`;
-    } else {
-        let fahrenheit = parseFloat(temperature);
-        let celsius = (fahrenheit - 32) * 5/9;
-        temperatureResult.textContent = `Result: ${celsius.toFixed(2)} °C`;
-    }
+  if (unit === "celsius") {
+    let celsius = parseFloat(temperature);
+    let fahrenheit = (celsius * 9) / 5 + 32;
+    temperatureResult.textContent = `Result: ${fahrenheit.toFixed(2)} °F`;
+  } else {
+    let fahrenheit = parseFloat(temperature);
+    let celsius = ((fahrenheit - 32) * 5) / 9;
+    temperatureResult.textContent = `Result: ${celsius.toFixed(2)} °C`;
+  }
 }
